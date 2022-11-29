a48007dadf5dbc591efc35864281fb398b225ebe
ptagliavia <79185768+ptagliavia@users.noreply.github.com>
2022-11-29 14:14:51 -0800

    i deed it

--- src/test/java/com/example/circleci/CircleCiApplicationTests.java
+++ src/test/java/com/example/circleci/CircleCiApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.circleci;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CircleCiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate template;
+
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    public void homeTest() {
+        //create request for local host
+        String url = "http://localhost:" +port + "/api/v1/home";
+
+        HttpEntity request = new HttpEntity(new HttpHeaders());
+
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(), "Hello, world!");
+    }
 }
